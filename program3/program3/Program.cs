01367d4a8577a876f13c52a5844b8b36e4141c97
ASHOKJAAT12 <ajat39963@gmail.com>
2026-07-11 00:08:09 +0530

    program 3

---
+++ program3/program3/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections;
+namespace name_3
+{
+    class program_3
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Enter number : ");
+            int a = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Your Number is : " + a);
+            
+        }
+    }
+}
