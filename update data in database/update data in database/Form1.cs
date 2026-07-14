52f9ac4b767f95750961e5950b50fe23987b2c09
ASHOKJAAT12 <ajat39963@gmail.com>
2026-07-15 00:02:09 +0530

    update value in database program add

---
+++ update data in database/update data in database/Form1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace update_data_in_database
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            SqlConnection con = new SqlConnection(@"data source=DESKTOP-GUSLB1E; initial catalog=juice; integrated security=true;");
+            con.Open();
+            string str = "select rollno from student1;";
+            SqlCommand cmd = new SqlCommand(str, con);
+            SqlDataReader dr = cmd.ExecuteReader();
+            while (dr.Read())
+            {
+                comboBox1.Items.Add(dr.GetValue(0));
+            }
+            con.Close();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            SqlConnection con = new SqlConnection(@"data source=DESKTOP-GUSLB1E; initial catalog=juice; integrated security=true;");
+            con.Open();
+            string str = "update student1 set name='" + tname.Text + "',class='"+tclass.Text+"',subject='"+tsubject.Text+"' where rollno="+comboBox1.Text+"";
+            SqlCommand cmd = new SqlCommand(str, con);
+            cmd.ExecuteNonQuery();
+            con.Close();
+        }
+    }
+}
